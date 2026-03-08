c0d28cc079b92cfa7a67cefbc9fe70d703814ef3
Glave <ngla@mail.ru>
2026-03-08 14:12:28 +0300

    connect to SonarQube

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("java")
     id("com.github.ben-manes.versions") version "0.53.0"
+    id("org.sonarqube") version "7.2.3.7755"
     application
 }
 
@@ -27,4 +28,11 @@ tasks.getByName("run", JavaExec::class) {
 
 application {
     mainClass = "hexlet.code.App"
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "PonomarevIE_java-project-61")
+        property("sonar.organization", "ponomarevie")
+    }
 }
