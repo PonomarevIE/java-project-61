32fcc8472e6c1627df229c22ee3d1283d9ee1a74
Glave <ngla@mail.ru>
2026-03-15 12:46:53 +0300

    add game Progression

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,6 +3,7 @@ package hexlet.code;
 import hexlet.code.games.Even;
 import hexlet.code.games.Calc;
 import hexlet.code.games.GCD;
+import hexlet.code.games.Progression;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -16,6 +17,7 @@ public class App {
         System.out.println("2 - Even");
         System.out.println("3 - Calc");
         System.out.println("4 - GCD");
+        System.out.println("5 - Progression");
         System.out.println("0 - Exit");
         System.out.print("Your choice: ");
 
@@ -42,6 +44,9 @@ public class App {
                 break;
             case "4":
                 GCD.run();
+                break;
+            case "5":
+                Progression.run();
                 break;
             default:
                 throw new InputMismatchException("Incorrect menu item: " + menuItem);
---
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -0,0 +1,56 @@
+package hexlet.code.games;
+
+import hexlet.code.Engine;
+
+import java.util.Random;
+
+public class Progression {
+    // Игра: "Арифметическая прогрессия"
+    private static final int MAX_NUMBER = 100;
+    private static final int PROGRESSION_LENGTH = 10;
+    private static final int PROGRESSION_MAX_START_NUMBER = 30;
+    private static final int PROGRESSION_MAX_STEP = 10;
+
+    public static void run() {
+        String gameIntroduction = "What number is missing in the progression?";
+        String[][] gameData = new String[Engine.GAME_ROUNDS_QTY][2];
+
+        Random rand = new Random();
+
+        // формирование вопросов и ответов
+        for (int roundNumber = 0; roundNumber < Engine.GAME_ROUNDS_QTY; roundNumber++) {
+            int firstNumber = rand.nextInt(MAX_NUMBER);
+            int secondNumber = rand.nextInt(MAX_NUMBER);
+
+            String[] progression = makeProgression();
+
+            int hiddenPosition = rand.nextInt(PROGRESSION_LENGTH);
+            gameData[roundNumber][Engine.DATA_ANSW_IND] = progression[hiddenPosition];
+
+            progression[hiddenPosition] = "..";
+            gameData[roundNumber][Engine.DATA_QUEST_IND] = String.join(" ", progression);
+        }
+
+        var engine = new Engine(gameIntroduction, gameData);
+
+        engine.runGame(); // запуск игры
+    }
+
+    private static String[] makeProgression() {
+        // метод формирует массив, содержащий арифметическую прогрессию
+        Random rand = new Random();
+        var progression = new String[PROGRESSION_LENGTH];
+
+        int step = rand.nextInt(PROGRESSION_MAX_STEP); // приращение
+
+        // первый элемент прогрессии определяется случайным образом
+        int start = rand.nextInt(PROGRESSION_MAX_START_NUMBER);
+
+        // остальные элементы вычисляются на основе выбранного приращения
+        for (int i = 0; i < PROGRESSION_LENGTH; i++) {
+            int current = start + i * step;
+            progression[i] = Integer.toString(current);
+        }
+        return progression;
+    }
+}
